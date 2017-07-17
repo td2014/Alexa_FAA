01a221e6cb9134b0e086b5f0224dc0b55fbb4181
td2014 <anthonyd2004@gmail.com>
2017-07-17 14:54:20 -0700

    Added test cases.

--- Alexa_FAA_data_helper.js
+++ Alexa_FAA_data_helper.js
@@ -5,6 +5,40 @@ var ENDPOINT = 'http://services.faa.gov/airport/status/';
 
 function AlexaFAADataHelper() {}
 
+AlexaFAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
+    var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.') ({
+        weather: airportStatus.weather.weather,
+        temp: airportStatus.weather.temp,
+        wind: airportStatus.weather.wind
+    });
+
+    if (airportStatus.delay === 'true') {
+        var template = _.template('There is currently a delay for ${airport}. ' +
+            'The average delay time is ${delay_time}. ' +
+            'Delay is because of the following: ${delay_reason}. ${weather}');
+
+        return template({
+      
+            airport: airportStatus.name,
+            delay_time: airportStatus.status.avgDelay,
+            delay_reason: airportStatus.status.reason,
+            weather: weather
+        });
+
+        } else {
+
+          // no delay
+
+          return _.template('There is currently no delay at ${airport}. ${weather}')({
+              airport: airportStatus.name,
+              weather: weather
+          });
+
+          }
+
+          };
+
+
 AlexaFAADataHelper.prototype.requestAirportStatus = function(airportCode) {
     return this.getAirportStatus(airportCode).then(
         function(response) {
